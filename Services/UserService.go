8c25b2a7e8028eb4bc7384aadfb402248eb19b57
vince <1130018964@qq.com>
2021-03-04 23:51:19 +0800

    测试

---
+++ Services/UserService.go
@@ -0,0 +1,16 @@
+package Services
+
+type IUserService interface {
+	GetName(userid int) string
+}
+
+type UserService struct {
+}
+
+func (this UserService) GetName(userid int) string {
+	if userid == 101 {
+		return "111"
+	}
+
+	return ""
+}
